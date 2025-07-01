a4257fe1b5ab016583c90f540340ebf9a63fc13b
GioGambale <82822383+GioGambale@users.noreply.github.com>
2025-07-01 18:59:17 +0200

    v1.1

--- myLibrary/flutter_service_worker.js
+++ myLibrary/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "146604dc506650f17e728958af600f78",
+const RESOURCES = {"flutter_bootstrap.js": "022eb37a78b8ebffec448140fd5b8d13",
 "version.json": "86f87385fec6b63fc6e044cda9e68568",
 "index.html": "04658c1ce4a995168e73eaaaf745d6df",
 "/": "04658c1ce4a995168e73eaaaf745d6df",
-"main.dart.js": "fc3b28ca0e8cad4c6564668fc26e41dc",
+"main.dart.js": "f8f93461951781abc255f4a0c36c8d52",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
